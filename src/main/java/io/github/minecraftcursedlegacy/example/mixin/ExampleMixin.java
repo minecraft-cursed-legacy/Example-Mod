2db696ad682c52f2e3090fb042e35a3979a5b556
valoeghese <valoeghesemods@gmail.com>
2020-05-04 23:03:05 +1200

    nearly ready

--- src/main/java/io/github/minecraftcursedlegacy/example/mixin/ExampleMixin.java
+++ src/main/java/io/github/minecraftcursedlegacy/example/mixin/ExampleMixin.java
@@ -1,4 +1,4 @@
-package io.github.minecraftcursedlegacy;
+package io.github.minecraftcursedlegacy.example.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
